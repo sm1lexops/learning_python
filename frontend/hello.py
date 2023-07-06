474e294526c7b82b1cf2f45ad214db004150d95f
sm1lexops <smiledevops@gmail.com>
2023-07-07 00:02:49 +0300

    impove hello.py

--- frontend/hello.py
+++ frontend/hello.py
@@ -3,3 +3,7 @@ import sys
 print("First create file `hello.py`")
 
 print(f"First we need install python {sys.version}.{sys.version_info}")
+print("What is your name dude?")
+name = input()
+print(f"Hello {name}")
+
