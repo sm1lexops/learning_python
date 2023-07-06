8b0c946b11d04f42409dc0c9d8e67dbefd134873
sm1lexops <smiledevops@gmail.com>
2023-07-06 23:24:28 +0300

    first command hello.py

--- frontend/hello.py
+++ frontend/hello.py
@@ -1,1 +1,5 @@
-print("hello, pls could you open the python programming world for me")
+import sys
+
+print("First create file `hello.py`")
+
+print(f"First we need install python {sys.version}.{sys.version_info}")
