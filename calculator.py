960d22f9d3aa681e66c4148fdeb34729502f7bcb
sm1lexops <smiledevops@gmail.com>
2023-07-07 12:21:42 +0300

    continue learning

---
+++ calculator.py
@@ -0,0 +1,5 @@
+x = float(input("Input x: "))
+y = float(input("Input y: "))
+z = round(x + y, 2)
+
+print(f"Summary x + y = {z:,}")
--- frontend/hello.py
+++ frontend/hello.py
@@ -4,6 +4,8 @@ print("First create file `hello.py`")
 
 print(f"First we need install python {sys.version}.{sys.version_info}")
 print("What is your name dude?")
-name = input()
-print(f"Hello {name}")
+name = input().strip().upper()
+first_name, last_name = name.split()
+
+print(f"Hello, {first_name} {last_name}")
 
